9f68064f7bfc35ade018eb5065927ede27a0d03b
Edward Thomson <ethomson@edwardthomson.com>
2019-02-15 08:47:26 +0000

    foo

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,11 +5,15 @@ set -e
 git config user.name "${GITHUB_ACTOR}"
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 
-cm doc api.docurium
-
-git checkout gh-pages
+env
 ls -Flas
+git status
+
+#cm doc api.docurium
+
+#git checkout gh-pages
+#ls -Flas
 
 #git push origin gh-pages
 
-echo "Updated gh-pages."
+#echo "Updated gh-pages."
